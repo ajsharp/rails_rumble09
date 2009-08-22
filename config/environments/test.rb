2518415c852d79d0c54b43ab915833e821cab61b
Alex Sharp <ajsharp@gmail.com>
2009-08-21 20:44:23 -0500

    added mocha should and factory_girl gem dependencies

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,3 +24,7 @@ config.action_mailer.delivery_method = :test
 # Restful Authentication
 REST_AUTH_SITE_KEY = 'f5945d1c74d3502f8a3de8562e5bf21fe3fec887'
 REST_AUTH_DIGEST_STRETCHES = 10
+
+config.gem 'mocha', :version => ">= 0.9.5"
+config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => "http://gems.github.com", :version => ">= 2.10.1"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com", :version => ">= 1.2.1"

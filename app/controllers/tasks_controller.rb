c6da25bfd7057f2fbca9c6377503a7966fc2fe22
Travis Black <travisb@vrazzle.com>
2009-08-21 23:11:27 -0500

    refactoring tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,16 @@
 class TasksController < ApplicationController
+  before_filter :login_required
+  
   def index
     @tasks = Task.find()
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(params[:task])
-    @task.creator_id = current_user.id
+    @task = current_user.tasks.new(params[:task])
     @task.save
   end
 end

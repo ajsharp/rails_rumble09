3c0a1414319a956105f7e0ab40bd2ae7918ab5f9
Travis Roberts <traviskroberts@gmail.com>
2009-08-21 22:15:52 -0500

    updating application layout and adding default site controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,8 @@ module ApplicationHelper
   # Outputs the corresponding flash message if any are set
   def flash_messages
     messages = []
-    %w(notice warning error).each do |msg|
-      messages << content_tag(:div, html_escape(flash[msg.to_sym]), :id => "flash-#{msg}") unless flash[msg.to_sym].blank?
+    %w(notice success error).each do |msg|
+      messages << content_tag(:div, html_escape(flash[msg.to_sym]), :id => "#{msg}") unless flash[msg.to_sym].blank?
     end
     messages
   end

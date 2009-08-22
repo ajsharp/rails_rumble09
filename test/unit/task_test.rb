bfdca5a096bd2b847fcd36ed4b7306f3a334db16
Alex Sharp <ajsharp@gmail.com>
2009-08-22 00:02:26 -0500

    added factory test in Task unit test

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  should_validate_presence_of :title, :creator_id
-  
-  should_belong_to :creator
+  context "a valid Task" do
+    should "have a valid Factory" do
+      assert Factory.build(:task).valid?
+      assert Factory(:task).save
+    end
+    
+    should_validate_presence_of :title, :creator_id  
+    should_belong_to :creator
+  end # end of a valid Task  
 end

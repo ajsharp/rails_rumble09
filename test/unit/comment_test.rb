d7108cbf0c395e1e5295ab659f0eeee077b3981f
Alex Sharp <ajsharp@gmail.com>
2009-08-21 22:27:30 -0500

    basic validations and associations tested

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  context "a valid Comment" do
+    should_belong_to :user
+    should_belong_to :task
+    
+    should_validate_presence_of :user_id, :task_id, :message
+  end # end of a valid Comment
+  
 end

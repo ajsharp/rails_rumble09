e32439ee1de20ca4bcd835b2c028c28bc3c88c7d
Travis Roberts <traviskroberts@gmail.com>
2009-08-22 18:02:25 -0500

    working on friend relationships

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -2,7 +2,7 @@ class ConnectionsController < ApplicationController
   before_filter :login_required, :get_current_user
   
   def index
-    @current_friends = @user.friends.paginate(:all, :page => params[:page], :per_page => 15)
+    @current_friends = @user.connections.approved.paginate(:all, :page => params[:page], :per_page => 15)
     @pending_friends = @user.connections.waiting_approval.paginate(:all, :page => params[:page], :per_page => 15)
     @friend_requests = Connection.all(:conditions => ["status = ? AND friend_id = ?", 'pending', @user.id])
   end
@@ -20,7 +20,7 @@ class ConnectionsController < ApplicationController
     end
     @connection = @user.connections.new(:friend_id => friend_user.id)
     if @connection.save
-      UserMailer.deliver_friend_request(@user, friend)
+      UserMailer.deliver_friend_request(@user, friend_user)
       flash[:success] = "A connection request has been sent to the user."
       redirect_to connections_path
     else
@@ -47,16 +47,9 @@ class ConnectionsController < ApplicationController
   
   def update
     @connection = Connection.find(params[:id])
-    respond_to do |format|
-      if @connection.update_attribute(:status, params[:response])
-        format.html { redirect_to connections_path }
-      else
-        format.html {
-          flash[:error] = "An error occured."
-          redirect_to connections_path
-        }
-      end
-    end
+    @connection.accept_friendship! if params[:response] == 'accept'
+    @connection.reject_friendship! if params[:response] == 'reject'
+    redirect_to connections_path
   end
   
   def destroy

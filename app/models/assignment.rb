1fc7c866fe5356106f2e5a38bde6dcf970509215
Travis Black <travisb@vrazzle.com>
2009-08-22 21:30:25 -0500

    adding changes for tasks templates and testing for assignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,24 +3,29 @@ class Assignment < ActiveRecord::Base
   include AASM::Persistence
   aasm_column :status
   
-  aasm_initial_state :unaccepted
-  aasm_state :unaccepted
+  aasm_initial_state :pending
+  aasm_state :pending
   aasm_state :accepted
   aasm_state :declined
+  aasm_state :passed
   aasm_state :completed
   
   aasm_event :accept_assignment do
-    transitions :to => :accepted, :from => :unaccepted
+    transitions :to => :accepted, :from => :pending
   end
   
   aasm_event :decline_assignment do
-    transitions :to => :declined, :from => :unaccepted
+    transitions :to => :declined, :from => :pending
   end
   
   aasm_event :complete_assignment do
     transitions :to => :completed, :from => :accepted
   end
   
+  aasm_event :pass_task do
+    transitions :to => :passed, :from => :accepted
+  end
+    
   belongs_to :assigner, :class_name => "User", :foreign_key => "assigner_id"
   belongs_to :assignee, :class_name => "User", :foreign_key => "assignee_id"
   belongs_to :task
@@ -28,4 +33,5 @@ class Assignment < ActiveRecord::Base
   validates_presence_of :assigner_id, :on => :create, :message => "can't be blank"
   validates_presence_of :assignee_id, :on => :create, :message => "can't be blank"
   validates_presence_of :task_id, :on => :create, :message => "can't be blank"
+
 end
